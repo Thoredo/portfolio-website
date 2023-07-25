7e4d0a3899e14828d94d6a90084f09be3c24a4c6
Jurgen Stegeman <jurgenstegeman@live.nl>
2023-07-25 10:48:17 +0200

    rename to Home.py

--- Home.py
+++ Home.py
@@ -6,16 +6,16 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=200)
+    st.image("images/photo.png", width=300)
 
 with col2:
     st.title("Jurgen Stegeman")
     content = """
-    Hello, I am Jurgen! I'm a starting developer, in my life I have done lots of 
-    different studies and jobs. But I always had an itch for programming, after 
-    a period of sickness I decided to use my time to learn something new.
-    I started learning HTML, CSS and JavaScript. I had a lot of fun learning them 
-    especially JS. But I wasn't sure what I wanted to do yet so after a 
+    Hello, I am Jurgen! I'm a starting developer, in my life I have done lots 
+    of different studies and jobs. But I always had an itch for programming, 
+    after a period of sickness I decided to use my time to learn something new.
+    I started learning HTML, CSS and JavaScript. I had a lot of fun learning 
+    them especially JS. But I wasn't sure what I wanted to do yet so after a 
     orientation program through Techgrounds, I started a data engineer course 
     through Bit Academy. I loved coding in Python so I decided to learn as 
     much as I could about Python.
